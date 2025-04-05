797e32baeb6e9a7975effaa5c875ff47dffee484
Anneta <annetadziedzom@gmail.com>
2025-04-05 21:24:01 +0000

    myCards.js

---
+++ myCards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native';
+const MyCards =() => {
+    return (
+        <View>
+            <Tex></Tex>
+           
+        </View>
+    );
+};
+
+export default MyCards;
